cc00edd4b6ee7b483576735dde72ddb5a2ab411d
lucianobs1 <lucianotetsu@gmail.com>
2023-03-03 19:18:03 -0300

    chore: add TransactionTable styles

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,32 @@
 import { Header } from '../../components/Header';
 import { Summary } from '../../components/Summary';
+import { TransactionsContainer, TransactionsTable } from './styles';
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>R$ 12.000,00</td>
+              <td>Venda</td>
+              <td>13/02/2023</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>- R$ 59,00</td>
+              <td>Alimentação</td>
+              <td>13/01/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
