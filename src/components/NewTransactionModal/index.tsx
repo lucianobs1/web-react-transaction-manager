46973c1905176f155e0d89f4e7f4f3f3604fc3b5
lucianobs1 <lucianotetsu@gmail.com>
2023-03-06 15:23:55 -0300

    chore: add TransactionTypeButton to controle incomes and outcomes

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,12 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { X } from 'phosphor-react';
-import { CloseButton, Content, Overlay } from './styles';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionsType,
+  TransactionsTypeButton,
+} from './styles';
 
 export function NewTransactionModal() {
   return (
@@ -17,6 +23,17 @@ export function NewTransactionModal() {
           <input type="text" placeholder="Descrição" required />
           <input type="number" placeholder="Preço" required />
           <input type="text" placeholder="Categoria" required />
+
+          <TransactionsType>
+            <TransactionsTypeButton variant="income">
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionsTypeButton>
+            <TransactionsTypeButton variant="outcome">
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionsTypeButton>
+          </TransactionsType>
 
           <button type="submit">Cadastrar</button>
         </form>
